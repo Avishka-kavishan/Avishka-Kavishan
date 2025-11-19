e0cfd104a54e1707ea2dca184d832a9f21ee1399
Avishka Kavishan <avishkakavishan13@gmail.com>
2025-11-19 21:54:41 +0530

    added home page's details

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,14 +6,12 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "UI/UX Designer",
+          "Product Designer"
         ],
         autoStart: true,
         loop: true,
-        deleteSpeed: 50,
+        deleteSpeed: 10,
       }}
     />
   );

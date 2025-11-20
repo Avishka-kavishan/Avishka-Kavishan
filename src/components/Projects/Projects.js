ac91eaf70692472fd819dc08c515f98bb7eee2a1
Avishka Kavishan <avishkakavishan13@gmail.com>
2025-11-21 02:28:07 +0530

    added the image for project's cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import leaf from "../../Assets/Projects/edu.jpg";
+import emotion from "../../Assets/Projects/himalaya.jpg";
+import editor from "../../Assets/Projects/laundry.jpg";
+import chatify from "../../Assets/Projects/booking.jpg";
+import suicide from "../../Assets/Projects/setty.jpg";
+import bitsOfCode from "../../Assets/Projects/travel.jpg";
 
 function Projects() {
   return (
